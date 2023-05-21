1d19982135950fca596d19a4e52cdc30b1022961
minhajul9 <110691447+minhajul9@users.noreply.github.com>
2023-05-21 10:29:05 +0600

    2 answers added in blogs

--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -10,6 +10,7 @@ import ToyDetails from "../components/pages/ToyDetails/ToyDetails";
 import MyToys from "../components/pages/MyToys/MyToys";
 import AddAToy from "../components/pages/AddAToy/AddAToy";
 import EditToys from "../components/pages/EditToys/EditToys";
+import Blogs from "../components/pages/Blogs";
 
 const router = createBrowserRouter([
     {
@@ -42,7 +43,10 @@ const router = createBrowserRouter([
                 element: <PrivateRoute><MyToys></MyToys></PrivateRoute>
             },
             {
+                path: 'blogs',
+                element: <Blogs></Blogs>
             },
+            
             {
                 path: 'editToys/:id',
                 element: <PrivateRoute><EditToys></EditToys></PrivateRoute>, 
